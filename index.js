3153bcdb310a1cdf0592672d401140b0e4edf945
Vitor <vitorm.ramires@hotmail.com>
2021-04-26 20:05:57 -0300

    arrumando

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 
 
+
+
+
 function Verificar(){
-
-var ano = new Date()
-var anoAtual = ano.getFullYear()
-var Nascimento = document.getElementById('ano')
-var msg = document.getElementById('msg')
-var msg2 = document.querySelector('.msg2')
-
-
-
-//tratamento de erro
-if( Number(Nascimento.value) > anoAtual || Nascimento.value.length === 0){
+    
+    var ano = new Date()
+    var anoAtual = ano.getFullYear()
+    var Nascimento = document.getElementById('ano')
+    var msg = document.getElementById('msg')
+    var msg2 = document.querySelector('.msg2')
+    
+    
+    
+    //tratamento de erro
+    if( Number(Nascimento.value) > anoAtual || Nascimento.value.length === 0){
         alert("Complete o campo corretamente")
-} else {
-
-    var idade = anoAtual - Number(Nascimento.value)
-    var sex = document.getElementsByName('gender')
-    var genero = ''
-    var imagem = document.createElement('img')
-    imagem.setAttribute('id','foto')
-
+    } else {
+        
+        var idade = anoAtual - Number(Nascimento.value)
+        var sex = document.getElementsByName('gender')
+        var genero = ''
+        var imagem = document.createElement('img')
+        imagem.setAttribute('id','foto')
 
         if(sex[0].checked){
             
         if(idade > 100 || idade < 0) {
-            alert('Idade invalida!')
+            alert('Idade invalida')
             msg.innerHTML = "Preencha corretamente!"
-            imagem.setAttribute('src', 'img/error.png')
+        
         } else {
 
            var genero = 'masculino'
@@ -55,7 +57,6 @@ if( Number(Nascimento.value) > anoAtual || Nascimento.value.length === 0){
             if(idade > 100 || idade < 0) {
                 alert('Idade invalida!')
                 msg.innerHTML = "Preencha corretamente!"
-                imagem.setAttribute('src', 'img/error.png')
             } else {
 
           var genero = 'feminino'
@@ -79,21 +80,19 @@ if( Number(Nascimento.value) > anoAtual || Nascimento.value.length === 0){
                 imagem.setAttribute('src', 'img/idosa-mulher.jpg')
             }
 
-            msg.innerHTML = `Eu tenho ${idade} anos e sou do genero ${genero}`
+                msg.innerHTML = `Eu tenho ${idade} anos e sou do genero ${genero}`
+        
         }
 
-
-          
-        }
+    }
        
         msg2.innerHTML=""
         Nascimento.value = ""
         msg2.appendChild(imagem)
 
-        }
-
-    }
-
+    }   
+    
+}
 
 
 
